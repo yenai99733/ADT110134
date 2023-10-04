13e037e424c42b9d1888e5b365406793bd42a802
yenai99733 <102508598+yenai99733@users.noreply.github.com>
2023-10-04 15:47:55 +0800

    Add files via upload

---
+++ 20231004_99.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# 99乘法表
+for i in range (1, 10):
+    for j in range (1,10):
+        product = i * j
+        print("%d * %d = %2d " % (i,j,product), end = "")
+    print()
